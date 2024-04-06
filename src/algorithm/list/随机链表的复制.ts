4f3629b61398de63c3295c95023a44aa22865fd2
yu <964494258@qq.com>
2024-04-06 13:26:05 +0800

    feat: temp

--- src/algorithm/list/随机链表的复制.ts
+++ src/algorithm/list/随机链表的复制.ts
@@ -6,3 +6,14 @@ class Node<T = number> extends TreeNode<T> {
 function copyRandomList(head: Node | null): Node | null {
   const res = new Node();
 }
+// s = "egg", t = "add"
+function isIsomorphic(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const smap: Map<string, number> = new Map();
+  const tmap: Map<string, number> = new Map();
+  for (const [i, ch] in [...s].entries) {
+    if(smap.has(ch)&&!tmap.has(ch))
+  }
+}
